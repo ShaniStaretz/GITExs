cd3026f1e7e9f4aee4eb0fa26db4ceb62f3ab666
Shani Staretz <shakimi88@gmail.com>
2018-12-23 19:12:02 +0200

    change script.js

---
+++ js/script.js
@@ -0,0 +1,71 @@
+var  populationArray=[];
+var maxPopulation;
+var isOverPopulated;
+
+populationArray=[500000,200000,90000,10000,70000];
+maxPopulation= 1000000;
+isOverPopulated=false;
+
+//check first condition on populationArray[0]
+if(populationArray[0]>maxPopulation)
+{
+console.log("larger");
+}
+else if(populationArray[0]===maxPopulation)
+{
+    console.log("equals");
+}
+else console.log("smaller");
+
+//check first condition on populationArray[1]
+if(populationArray[1]>maxPopulation)
+{
+console.log("larger");
+}
+else if(populationArray[1]===maxPopulation)
+{
+    console.log("equals");
+}
+else console.log("smaller");
+
+//check first condition on populationArray[2]
+if(populationArray[2]>maxPopulation)
+{
+console.log("larger");
+}
+else if(populationArray[2]===maxPopulation)
+{
+    console.log("equals");
+}
+else console.log("smaller");
+
+//check first condition on populationArray[3]
+if(populationArray[3]>maxPopulation)
+{
+console.log("larger");
+}
+else if(populationArray[3]===maxPopulation)
+{
+    console.log("equals");
+}
+else console.log("smaller");
+//check first condition on populationArray[4]
+if(populationArray[4]>maxPopulation)
+{
+console.log("larger");
+}
+else if(populationArray[4]===maxPopulation)
+{
+    console.log("equals");
+}
+else console.log("smaller");
+
+if(populationArray[2]< 100,000 && populationArray[3] <100,000 && populationArray[4]<100,000 && maxPopulation < 100,000 || isMostPopulated === false)
+{
+    
+        console.log("Small cities!!!");
+ }
+ else {
+        console.log ("Large cities!!!");
+
+}
